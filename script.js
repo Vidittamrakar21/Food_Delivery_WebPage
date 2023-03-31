fd7547ac877d677aaf1177a096fea19eb44ff4d8
Vidit Tamrakar <114985411+Vidittamrakar21@users.noreply.github.com>
2023-03-31 23:21:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function pop() {
+    alert(
+      "We use cookies to personalize your experience. By continuing to visit this website you agree to our use of cookies."
+    );
+    // let name = prompt("what is your name !","Guest");
+    // console.log(name);
+  }
+  setTimeout(pop, 5000);
+
+ function lugin(){
+    let my = document.getElementById("rum")
+    my.style.display = "block"
+    
+    
+} 
+
+function cross(){
+    let my = document.getElementById("rum")
+    my.style.display = "none"
+   
+
+ }
